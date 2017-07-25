a331fd4799e1c5110af70c114244d4f455ac6f0d
Keylonnie Miller <keylonniem2017@gmail.com>
2017-07-25 11:02:35 -0400

    Functioning Age If Statement

--- bliss/main.py
+++ bliss/main.py
@@ -14,18 +14,19 @@ class HomePage(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("home.html")
         relationship_template = jinja_environment.get_template("Relationship.html")
+        finance_template = jinja_environment.get_template("Finance.html")
         concern_value = self.request.get('Concern')
         logging.info('HomePage handler got concern_value: ' + concern_value)
         if concern_value == "Relationships":
             self.response.write(relationship_template.render())
+        elif concern_value == "Finance":
+            self.response.write(finance_template.render({
+            'Age' : self.request.get("Age")
+            }))
         else:
             self.response.write(template.render({
             'Name' : self.request.get('Name'),
             'Concern' : self.request.get('Concern')}))
-
-
-
-
 
 
 app = webapp2.WSGIApplication([
