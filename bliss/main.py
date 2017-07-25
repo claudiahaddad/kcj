38be3d91bd9d5aff20295b90487ce5bdb2e2f89e
Keylonnie Miller <keylonniem2017@gmail.com>
2017-07-25 16:48:35 -0400

    Menu option at the bottom

--- bliss/main.py
+++ bliss/main.py
@@ -9,15 +9,17 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 class EntryPage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('entry.html')
+        template = jinja_environment.get_template('EntryPage/entry.html')
         self.response.out.write(template.render())
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template("home.html")
+        template = jinja_environment.get_template("Home/home.html")
         relationship_template = jinja_environment.get_template("Relationship/Relationship.html")
         finance_template = jinja_environment.get_template("Finance/Finance.html")
         health_template = jinja_environment.get_template("Health/Health.html")
+        career_template = jinja_environment.get_template("Career/Career.html")
+        school_template = jinja_environment.get_template("School/School.html")
         concern_value = self.request.get('Concern')
         logging.info('HomePage handler got concern_value: ' + concern_value)
         if concern_value == "Relationships":
@@ -36,6 +38,18 @@ class HomePage(webapp2.RequestHandler):
             'Concern' : self.request.get('Concern'),
             'Age' : self.request.get("Age")
             }))
+        elif concern_value == "Career":
+            self.response.write(career_template.render({
+            'Name' : self.request.get('Name'),
+            'Concern' : self.request.get('Concern'),
+            'Age' : self.request.get("Age")
+            }))
+        elif concern_value == "School":
+            self.response.write(school_template.render({
+            'Name' : self.request.get('Name'),
+            'Concern' : self.request.get('Concern'),
+            'Age' : self.request.get("Age")
+            }))
         else:
             self.response.write(template.render({
             'Name' : self.request.get('Name'),
@@ -43,7 +57,7 @@ class HomePage(webapp2.RequestHandler):
 
 class SleepApp(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template("sleepapp.html")
+        template = jinja_environment.get_template("Sleep/sleepapp.html")
         self.response.write(template.render({
         'Name' : self.request.get('Name'),
         'Wake-Up': self.request.get('Wake-Up'),
@@ -61,18 +75,18 @@ class MainHandler(webapp2.RequestHandler):
         query = Person.query()
         query = query.order(Person.name)
         people = query.fetch()
-        template = jinja_environment.get_template('templates/facts.html')
+        template = jinja_environment.get_template('facts.html')
         self.response.write(template.render({"name" : Person}))
 
 class NewEventHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/water_input.html')
+        template = jinja_environment.get_template('Water/water_input.html')
         self.response.write(template.render())
 
 
 class ConfirmationHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/confirmation.html')
+        template = jinja_environment.get_template('Water/confirmation.html')
         self.response.write(template.render({
             "name" : self.request.get("name"),
             "age" : self.request.get("age"),
@@ -84,7 +98,7 @@ class ResultHandler(webapp2.RequestHandler):
     def get(self):
 
         age = self.request.get("age")
-        age = int(age)
+        age = int("age")
         gender = self.request.get("gender")
         weight = self.request.get("weight")
         weight = int(weight)
@@ -98,7 +112,7 @@ class ResultHandler(webapp2.RequestHandler):
         else:
             water = ("???")
 
-        template = jinja_environment.get_template('templates/facts.html')
+        template = jinja_environment.get_template('facts.html')
         self.response.write(template.render({
             "name" : self.request.get("name"),
             "water" : "{:.0f}".format(water)
