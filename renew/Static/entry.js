d9a5227a4ea3cb01b7010485921ab8c1720b47f8
Keylonnie Miller <keylonniem2017@gmail.com>
2017-07-27 17:36:17 -0400

    Final Product??

--- renew/Static/entry.js
+++ renew/Static/entry.js
@@ -1,14 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 
 function showSlides() {
     var i;
@@ -19,5 +8,16 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 6000); // Change image every 2 seconds
+    setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+
+var slideIndex = 1;
+//showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
